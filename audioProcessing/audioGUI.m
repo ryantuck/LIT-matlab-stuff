f2566dd32e7a080068d1c7f8abab26da3f8bf60e
ryantuck <ryanstephentuck@gmail.com>
2013-12-05 14:09:48 -0500

    Add radio buttons

--- audioProcessing/audioGUI.m
+++ audioProcessing/audioGUI.m
@@ -2,7 +2,21 @@ function audioGUI
 
 % Audio UI to analyze headband audio data
 
+%  Create and then hide the GUI as it is being constructed.
+f = figure('Visible','off','Position',[360,500,450,285]);
 
+% create radio buttons
+hpot = uicontrol('Style','radiobutton',...
+            'String','pot value',...
+            'Position',[315,220,150,25]);
+hnum = uicontrol('Style','radiobutton',...
+            'String','number',...
+            'Position',[315,180,150,25]);
+hbr = uicontrol('Style','radiobutton',...
+            'String','brightness',...
+            'Position',[315,140,150,25]);
+
+set(f,'Visible','on');
 
 
 
